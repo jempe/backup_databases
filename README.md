e081274967fbd1a8aacfac5a9b4f8cfd6dac46b9
Jempe <info@jempe.org>
2024-04-04 13:49:23 +0000

    Postgres script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# backup_databases
+#Backup Databases Cron Scripts
+
+This is a collection of scripts that can be used to backup databases. The scripts are written in Bash and can be used to backup MySQL and PostgreSQL databases. The scripts are designed to be run as cron jobs.
+
+For additional information on how to use the scripts, please refer to the README files in the respective directories.
+
+[MySQL Backup Scripts](/jempe/backup_databases/tree/main/mysql)
+
+[PostgreSQL Backup Scripts](/jempe/backup_databases/tree/main/postgres)
---
+++ postgres/postgres_backup.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+ALLBACKUPS_DIR="/srv/backups"
+POSTGRES_BACKUP_DIR="$ALLBACKUPS_DIR/postgres"
+CURRENT_USER=$(whoami)
+
+
+BACKUPS_DIR=$POSTGRES_BACKUP_DIR/$CURRENT_USER
+
+
+if [ ! -d $BACKUPS_DIR ]; then
+  echo "Backup directory $BACKUPS_DIR does not exist. Please create folder $BACKUPS_DIR"
+  exit 1
+fi
+
+CURRENT_DATE=$(date +"%Y-%m-%d")
+CURRENT_DATETIME=$(date +"%Y-%m-%d-%H-%M-%S")
+
+echo "RUNNING BACKUP FOR POSTGRES DATABASE $CURRENT_USER AT $CURRENT_DATETIME"
+
+LATEST_BACKUP_LINK=$BACKUPS_DIR/latest.pg_dump
+
+BACKUP_FILE=$BACKUPS_DIR/$CURRENT_USER-$CURRENT_DATE.pg_dump
+
+pg_dump -Fc --username="$CURRENT_USER" --dbname="$CURRENT_USER" > $BACKUP_FILE
+
+if [ -f $LATEST_BACKUP_LINK ]; then
+  unlink $LATEST_BACKUP_LINK
+fi
+
+ln -s $BACKUP_FILE $LATEST_BACKUP_LINK
